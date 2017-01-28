3cad17b7c7acbb146e460ce2efdc52866109cd1e
Richard Epps <you@example.com>
2017-01-28 14:37:20 -0500

    linearscales for d3

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var linearScale = d3.scaleLinear()
+	.domain([0,100])
+	.range([0,10])
+	.clamp(true);
+
+console.log(linearScale(-20));
+console.log(linearScale(0));
+console.log(linearScale(50));
+console.log(linearScale(100));
+console.log(linearScale(120));
+
+console.log(linearScale.invert(.5));
